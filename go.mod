c6fc4330a149a9ec5c8d7d61a2072d7b183e3f80
resxarcher <zero0daylogic@gmail.com>
2024-03-02 18:47:05 +0330

    [staging] commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Resxarcher/fbypass
 
 go 1.21.6
+
+require gopkg.in/yaml.v2 v2.4.0
--- main.go
+++ main.go
@@ -2,21 +2,54 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
-func bypass(d string) {
+type yamlData struct {
+	Method  string `yaml:"method"`
+	Headers string `yaml:"headers"`
+}
 
+func bypass(d string) {
+	// req, err := http.NewRequest()
+	// client := &http.Client{}
+
+	yamlMap := make(map[string]interface{})
+	yamlFile, err := os.ReadFile("/home/hunter/.config/fbypass/config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = yaml.Unmarshal(yamlFile, yamlMap)
+	if err != nil {
+		fmt.Println("err during unmarshaling $HOME/.config/fbypass/config.yaml:\n", err)
+		os.Exit(0)
+	}
+	for _, item := range yamlMap {
+		fmt.Println(item)
+	}
 }
 
 func main() {
 	var (
-		domain      string
-		lists       string
+		host        string
+		hosts       string
 		concurrency int
 	)
-	flag.StringVar(&domain, "domain", "", "single domain to check")
-	flag.StringVar(&lists, "lists", "", "file path contains domains to check")
+	flag.StringVar(&host, "host", "", "single host to check")
+	flag.StringVar(&hosts, "hosts", "", "file path contains hosts to check")
 	flag.IntVar(&concurrency, "c", 10, "number of concurrency")
 	flag.Parse()
+	if host != "" && hosts == "" {
+		bypass(host)
+	}
 
+	if hosts == "" && host == "" {
+		fmt.Println("you must provide a host or list of hosts.")
+		flag.Usage()
+		os.Exit(0)
+	}
 }
